2f9a404b70dcdb706db5494aafc9ca222e4c4d4f
GEOkettle <jsonmiou@gmail.com>
2022-05-31 01:31:40 +0900

    배울게 많너

--- react_ts_tutorial/src/App.tsx
+++ react_ts_tutorial/src/App.tsx
@@ -9,11 +9,28 @@ function HeadingWIthContent({ children }: { children: ReactNode }): ReactElement
     return <h1>{children}</h1>
 }
 // default props
+const defaultContainierProps = {
+    heading: <strong>My heading</strong>,
+    footer : <strong>My footer</strong>
+}
+type ContainerProps = { children: ReactNode} & typeof defaultContainierProps
+
+function Container({ heading, children,footer }: ContainerProps): ReactElement{ 
+    return(
+        <div>
+            <h1>{heading}</h1>
+            {children}
+            <h1>{footer}</h1>
+        </div>
+    )
+}
+Container.defaultProps=defaultContainierProps
 function App() {
   return (
     <div className="App">
           <Heading title={'hillow WOrld'} />
           <HeadingWIthContent><strong>fsdfdsa</strong></HeadingWIthContent>
+          <Container>Hello my bello</Container>
     </div>
   );
 }
